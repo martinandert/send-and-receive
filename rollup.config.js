d2177ca7b7624a21813f494a7f531018c7b890df
martinandert <mandert@gmail.com>
2017-09-10 14:02:02 +0200

    No need for a separate CommonJS build

--- rollup.config.js
+++ rollup.config.js
@@ -10,17 +10,10 @@ export default [
     ],
 
     output: {
-      file: pkg.browser,
+      file: pkg.main,
       format: 'umd',
       name: 'sar',
       sourcemap: true,
-    }
-  }, {
-    input: 'src/index.js',
-
-    output: {
-      file: pkg.main,
-      format: 'cjs',
     }
   }, {
     input: 'src/index.js',
@@ -30,31 +23,4 @@ export default [
       format: 'es',
     }
   }
-]
-
-//   // browser-friendly UMD build
-//   {
-//     entry: 'src/main.js',
-//     dest: pkg.browser,
-//     format: 'umd',
-//     moduleName: 'howLongUntilLunch',
-//     plugins: [
-//       resolve(), // so Rollup can find `ms`
-//       commonjs() // so Rollup can convert `ms` to an ES module
-//     ]
-//   },
-
-//   // CommonJS (for Node) and ES module (for bundlers) build.
-//   // (We could have three entries in the configuration array
-//   // instead of two, but it's quicker to generate multiple
-//   // builds from a single configuration where possible, using
-//   // the `targets` option which can specify `dest` and `format`)
-//   {
-//     entry: 'src/main.js',
-//     external: ['ms'],
-//     targets: [
-//       { dest: pkg.main, format: 'cjs' },
-//       { dest: pkg.module, format: 'es' }
-//     ]
-//   }
-// ];
+];
